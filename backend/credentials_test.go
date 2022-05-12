914b8aaad455d6153b0a33ac9a815fbd899cfdf7
AgustinSRG <41982624+AgustinSRG@users.noreply.github.com>
2022-05-12 16:18:26 +0200

    Test for credentials manager

---
+++ backend/credentials_test.go
@@ -0,0 +1,105 @@
+// Test for credentials manager
+
+package main
+
+import (
+	"crypto/subtle"
+	"os"
+	"path"
+	"testing"
+)
+
+func TestCreadentialsManager(t *testing.T) {
+	test_path_base := "./temp"
+
+	SetTempFilesPath(test_path_base)
+
+	err := os.MkdirAll(test_path_base, 0755)
+
+	if err != nil {
+		t.Error(err)
+		panic(err)
+	}
+
+	test_file := path.Join(test_path_base, "credentials.json")
+
+	// Initalize credentials
+
+	var cred VaultCredentialsManager
+
+	err = cred.Initialize(test_file)
+
+	if err != nil {
+		t.Error(err)
+		panic(err)
+	}
+
+	// Unlock
+
+	err = cred.UnlockVault(VAULT_DEFAULT_USER, VAULT_DEFAULT_PASSWORD)
+
+	if err != nil {
+		t.Error(err)
+		panic(err)
+	}
+
+	// Get key
+
+	key := cred.GetKey()
+
+	if key == nil {
+		t.Errorf("Invalid key received")
+	}
+
+	// Change password
+
+	err = cred.SetCredentials("user", "password", key)
+
+	if err != nil {
+		t.Error(err)
+		panic(err)
+	}
+
+	// Save
+
+	err = cred.SaveCredentials()
+
+	// Create new manager (simulate restart)
+
+	var cred2 VaultCredentialsManager
+
+	cred2.Initialize(test_file)
+
+	// Check password
+
+	b := cred2.CheckCredentials("user", "password")
+
+	if !b {
+		t.Errorf("Invalid password, but the password was valid")
+	}
+
+	b = cred2.CheckCredentials("user", "passwodd")
+
+	if b {
+		t.Errorf("Valid password, but the password was invalid")
+	}
+
+	err = cred.UnlockVault("user", "password")
+
+	if err != nil {
+		t.Error(err)
+		panic(err)
+	}
+
+	otherKey := cred.GetKey()
+
+	if otherKey == nil {
+		t.Errorf("Invalid key received")
+	}
+
+	if subtle.ConstantTimeCompare(otherKey, key) != 1 {
+		t.Errorf("Keys are different")
+	}
+
+	os.Remove(test_file)
+}
