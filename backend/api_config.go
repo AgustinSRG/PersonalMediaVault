cea954d701f99efce9bfc8fe1e129afb00d5d097
AgustinSRG <41982624+AgustinSRG@users.noreply.github.com>
2022-05-20 23:24:40 +0200

    No cache for json GET APIs

--- backend/api_config.go
+++ backend/api_config.go
@@ -34,6 +34,7 @@ func api_getConfig(response http.ResponseWriter, request *http.Request) {
 	}
 
 	response.Header().Add("Content-Type", "application/json")
+	response.Header().Add("Cache-Control", "no-cache")
 	response.WriteHeader(200)
 
 	response.Write(jsonResult)
