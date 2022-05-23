1b84065e107c6661d20480baf7c93ec959ec16cc
AgustinSRG <41982624+AgustinSRG@users.noreply.github.com>
2022-05-23 15:57:09 +0200

    Add function to get media info for API lists

--- backend/api_media.go
+++ backend/api_media.go
@@ -2,7 +2,58 @@
 
 package main
 
-import "net/http"
+import (
+	"fmt"
+	"net/http"
+)
+
+type MediaListAPIItem struct {
+	Id        uint64    `json:"id"`
+	Type      MediaType `json:"type"`
+	Title     string    `json:"title"`
+	Thumbnail string    `json:"thumbnail"`
+	Duration  float64   `json:"duration"`
+	Width     int32     `json:"width"`
+	Height    int32     `json:"height"`
+}
+
+func GetMediaMinInfo(media_id uint64, session *ActiveSession) *MediaListAPIItem {
+	var result MediaListAPIItem
+
+	result.Id = media_id
+
+	media := GetVault().media.AcquireMediaResource(media_id)
+
+	meta, err := media.ReadMetadata(session.key)
+
+	if err != nil {
+		LogError(err)
+	}
+
+	if meta != nil {
+		result.Type = meta.Type
+		result.Title = meta.Title
+		if meta.ThumbnailReady {
+			result.Thumbnail = "/assets/b/" + fmt.Sprint(media_id) + "/" + fmt.Sprint(meta.ThumbnailAsset) + "/thumbnail.jpg"
+		} else {
+			result.Thumbnail = ""
+		}
+		result.Duration = meta.MediaDuration
+		result.Width = meta.Width
+		result.Height = meta.Height
+	} else {
+		result.Type = MediaTypeDeleted
+		result.Title = ""
+		result.Thumbnail = ""
+		result.Duration = 0
+		result.Width = 0
+		result.Height = 0
+	}
+
+	GetVault().media.ReleaseMediaResource(media_id)
+
+	return &result
+}
 
 func api_getMedia(response http.ResponseWriter, request *http.Request) {
 
