3b907df2dad7cfb15ed14122bea7d21f29aff376
AgustinSRG <41982624+AgustinSRG@users.noreply.github.com>
2022-05-28 17:48:53 +0200

    Add timeouts util

---
+++ frontend/src/utils/timeout.ts
@@ -0,0 +1,23 @@
+// Timeouts manager
+
+export class Timeouts {
+
+    public static pending: {[key: string]: number} = {};
+
+
+    public static Set(key: string, ms: number, handler: () => void) {
+        Timeouts.Abort(key);
+        Timeouts.pending[key] = setTimeout(() => {
+            delete Timeouts.pending[key];
+            handler();
+        }, ms);
+    }
+
+
+    public static Abort(key: string) {
+        if (Timeouts.pending[key] !== undefined) {
+            clearTimeout(Timeouts.pending[key]);
+            delete Timeouts.pending[key];
+        }
+    }
+}
