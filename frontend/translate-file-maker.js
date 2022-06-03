63595dd6a397169e1033b6e6b13ce393fdaad9f7
AgustinSRG <41982624+AgustinSRG@users.noreply.github.com>
2022-06-03 16:27:39 +0200

    Add script to auto add missing i18n keys

---
+++ frontend/translate-file-maker.js
@@ -0,0 +1,137 @@
+// Translation file maker
+// Search for uses of translation function and adds them to translation files
+
+'use strict';
+
+const FS = require("fs");
+const Path = require("path");
+
+function generateTranslationConfigurationFile(lang, resources) {
+    const keys = Object.keys(resources).sort();
+    const result = Object.create(null);
+
+    for (const key of keys) {
+        result[key] = resources[key] || key;
+    }
+
+    return JSON.stringify(result, null, 2)
+}
+
+function getResourcesFromTranslationsFile(file) {
+    const locale = JSON.parse(FS.readFileSync(file).toString());
+    return locale;
+}
+
+function searchTranslationUsages(file) {
+    const str = FS.readFileSync(file).toString();
+
+    const matches = (str.match(/\$t\(\"([^\\"]*)\"\)/gi) || []).concat((str.match(/\$t\(\'[^\\']*\'\)/gi) || []));
+    const usages = {};
+
+    for (const match of matches) {
+        const tKey = JSON.parse('"' + match.substr(4, match.length - 6) + '"');
+
+        usages[tKey] = "";
+    }
+
+    return usages;
+}
+
+function mergeResources(resource1, resource2) {
+    const result = {};
+
+    for (let key in resource1) {
+        result[key] = resource1[key] || resource2[key];
+    }
+
+    for (let key in resource2) {
+        result[key] = resource1[key] || resource2[key];
+    }
+
+    return result;
+}
+
+function mergeResourcesFirstSide(resource1, resource2) {
+    const result = {};
+
+    for (let key in resource1) {
+        result[key] = resource1[key] || resource2[key];
+    }
+
+    return result;
+}
+
+function findTranslationConfigFiles() {
+    const files = FS.readdirSync(Path.resolve(__dirname, "src/locales"));
+    const result = [];
+    for (let file of files) {
+        if (file.endsWith(".json")) {
+            result.push(Path.resolve(__dirname, "src/locales", file));
+        }
+    }
+    return result;
+}
+
+function getLangFromFile(file) {
+    return Path.basename(file).split(".")[0];
+}
+
+function scanDirectories(root) {
+    const files = [];
+    const filesInDir = FS.readdirSync(root);
+
+    for (const file of filesInDir) {
+        const absFile = Path.resolve(root, file);
+        const relFile = absFile.substr(Path.resolve(__dirname).length).substr(1);
+
+        if (relFile === ".git") {
+            continue;
+        }
+
+        const stats = FS.lstatSync(absFile);
+        if (stats.isDirectory()) {
+            const subFiles = scanDirectories(absFile);
+            for (const sf of subFiles) {
+                files.push(sf);
+            }
+        } else if (stats.isFile()) {
+            if (file.endsWith(".ts") || file.endsWith(".js") || file.endsWith(".vue")) {
+                files.push(absFile);
+            }
+        }
+    }
+
+    return files;
+}
+
+function main() {
+    console.log("Generating language files...");
+
+    const translationFiles = findTranslationConfigFiles();
+    const scanned = scanDirectories(Path.resolve(__dirname, 'src'));
+
+    // Scan directories looking for translation keys
+    let usedKeys = Object.create(null);
+
+    for (const file of scanned) {
+        const fileKeys = searchTranslationUsages(file);
+        if (Object.keys(fileKeys).length > 0) {
+            // console.log("[REPORT] Found " + Object.keys(fileKeys).length + " translation keys in file " + file);
+            // console.log("[KEYS-REPORT]" + JSON.stringify(Object.keys(fileKeys)));
+            usedKeys = mergeResources(usedKeys, fileKeys);
+        }
+    }
+
+    // console.log("[REPORT] Total used keys found: " + Object.keys(usedKeys).length);
+
+    // Regenerate translations files
+    for (const file of translationFiles) {
+        const keysFixed = mergeResourcesFirstSide(usedKeys, getResourcesFromTranslationsFile(file));
+
+        const content = generateTranslationConfigurationFile(Path.basename(file), keysFixed);
+
+        FS.writeFileSync(file, content);
+    }
+}
+
+main();
