527b5b5e730c1328813eda1291ec5a52b84aa0e6
AgustinSRG <41982624+AgustinSRG@users.noreply.github.com>
2022-07-12 10:57:24 +0200

    Update debug script

--- backend/rundaemon.bat
+++ backend/rundaemon.bat
@@ -2,5 +2,6 @@
 
 SET FFMPEG_PATH=/ffmpeg/bin/ffmpeg.exe
 SET FFPROBE_PATH=/ffmpeg/bin/ffprobe.exe
+SET FRONTEND_PATH=../frontend/dist/
 
 call pmv.exe --clean --daemon --debug --log-requests --cors-insecure
