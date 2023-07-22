390f63bf92be5e60fa5881d06f959c6a92b0798d
AgustinSRG <agustinsanromanguzman@gmail.com>
2023-07-22 13:21:26 +0200

    Delete extended description asset if empty

--- backend/api_media_extended_description.go
+++ backend/api_media_extended_description.go
@@ -47,118 +47,182 @@ func api_setExtendedDescription(response http.ResponseWriter, request *http.Requ
 
 	assetData := []byte(p.ExtendedDescription)
 
-	// Encrypt the description file
+	if len(assetData) > 0 {
+		// Encrypt the description file
 
-	desc_encrypted_file, err := EncryptAssetData(assetData, session.key)
+		desc_encrypted_file, err := EncryptAssetData(assetData, session.key)
 
-	if err != nil {
-		LogError(err)
+		if err != nil {
+			LogError(err)
 
-		ReturnAPIError(response, 500, "INTERNAL_ERROR", "Internal server error, Check the logs for details.")
-		return
-	}
-
-	// Put the description into the media assets
-
-	media := GetVault().media.AcquireMediaResource(media_id)
-
-	if media == nil {
-		os.Remove(desc_encrypted_file)
-		ReturnAPIError(response, 404, "NOT_FOUND", "Media not found")
-		return
-	}
-
-	meta, err := media.StartWrite(session.key)
-
-	if err != nil {
-		LogError(err)
-
-		os.Remove(desc_encrypted_file)
-
-		GetVault().media.ReleaseMediaResource(media_id)
-
-		ReturnAPIError(response, 500, "INTERNAL_ERROR", "Internal server error, Check the logs for details.")
-		return
-	}
-
-	if meta == nil {
-		media.CancelWrite()
-		os.Remove(desc_encrypted_file)
-		GetVault().media.ReleaseMediaResource(media_id)
-		ReturnAPIError(response, 404, "NOT_FOUND", "Media not found")
-		return
-	}
-
-	desc_asset := meta.NextAssetID
-	meta.NextAssetID++
-
-	success, asset_path, asset_lock := media.AcquireAsset(desc_asset, ASSET_SINGLE_FILE)
-
-	if !success {
-		media.CancelWrite()
-		os.Remove(desc_encrypted_file)
-		GetVault().media.ReleaseMediaResource(media_id)
-
-		ReturnAPIError(response, 404, "NOT_FOUND", "Media not found")
-		return
-	}
-
-	asset_lock.RequestWrite()
-	asset_lock.StartWrite()
-
-	// Move temp file
-	err = RenameAndReplace(desc_encrypted_file, asset_path)
-
-	asset_lock.EndWrite()
-
-	media.ReleaseAsset(desc_asset)
-
-	if err != nil {
-		LogError(err)
-
-		media.CancelWrite()
-		os.Remove(desc_encrypted_file)
-		GetVault().media.ReleaseMediaResource(media_id)
-
-		ReturnAPIError(response, 500, "INTERNAL_ERROR", "Internal server error, Check the logs for details.")
-		return
-	}
-
-	// Change metadata
-
-	if meta.HasExtendedDescription {
-		// Remove old asset
-		oldAsset := meta.ExtendedDescriptionAsset
-		success, asset_path, asset_lock = media.AcquireAsset(oldAsset, ASSET_SINGLE_FILE)
-
-		if success {
-			asset_lock.RequestWrite()
-			asset_lock.StartWrite()
-
-			os.Remove(asset_path)
-
-			asset_lock.EndWrite()
-
-			media.ReleaseAsset(oldAsset)
+			ReturnAPIError(response, 500, "INTERNAL_ERROR", "Internal server error, Check the logs for details.")
+			return
 		}
-	}
 
-	meta.HasExtendedDescription = true
-	meta.ExtendedDescriptionAsset = desc_asset
+		// Put the description into the media assets
 
-	// Save
-	err = media.EndWrite(meta, session.key, false)
+		media := GetVault().media.AcquireMediaResource(media_id)
 
-	if err != nil {
-		LogError(err)
+		if media == nil {
+			os.Remove(desc_encrypted_file)
+			ReturnAPIError(response, 404, "NOT_FOUND", "Media not found")
+			return
+		}
+
+		meta, err := media.StartWrite(session.key)
+
+		if err != nil {
+			LogError(err)
+
+			os.Remove(desc_encrypted_file)
+
+			GetVault().media.ReleaseMediaResource(media_id)
+
+			ReturnAPIError(response, 500, "INTERNAL_ERROR", "Internal server error, Check the logs for details.")
+			return
+		}
+
+		if meta == nil {
+			media.CancelWrite()
+			os.Remove(desc_encrypted_file)
+			GetVault().media.ReleaseMediaResource(media_id)
+			ReturnAPIError(response, 404, "NOT_FOUND", "Media not found")
+			return
+		}
+
+		desc_asset := meta.NextAssetID
+		meta.NextAssetID++
+
+		success, asset_path, asset_lock := media.AcquireAsset(desc_asset, ASSET_SINGLE_FILE)
+
+		if !success {
+			media.CancelWrite()
+			os.Remove(desc_encrypted_file)
+			GetVault().media.ReleaseMediaResource(media_id)
+
+			ReturnAPIError(response, 404, "NOT_FOUND", "Media not found")
+			return
+		}
+
+		asset_lock.RequestWrite()
+		asset_lock.StartWrite()
+
+		// Move temp file
+		err = RenameAndReplace(desc_encrypted_file, asset_path)
+
+		asset_lock.EndWrite()
+
+		media.ReleaseAsset(desc_asset)
+
+		if err != nil {
+			LogError(err)
+
+			media.CancelWrite()
+			os.Remove(desc_encrypted_file)
+			GetVault().media.ReleaseMediaResource(media_id)
+
+			ReturnAPIError(response, 500, "INTERNAL_ERROR", "Internal server error, Check the logs for details.")
+			return
+		}
+
+		// Change metadata
+
+		if meta.HasExtendedDescription {
+			// Remove old asset
+			oldAsset := meta.ExtendedDescriptionAsset
+			success, asset_path, asset_lock = media.AcquireAsset(oldAsset, ASSET_SINGLE_FILE)
+
+			if success {
+				asset_lock.RequestWrite()
+				asset_lock.StartWrite()
+
+				os.Remove(asset_path)
+
+				asset_lock.EndWrite()
+
+				media.ReleaseAsset(oldAsset)
+			}
+		}
+
+		meta.HasExtendedDescription = true
+		meta.ExtendedDescriptionAsset = desc_asset
+
+		// Save
+		err = media.EndWrite(meta, session.key, false)
+
+		if err != nil {
+			LogError(err)
+
+			GetVault().media.ReleaseMediaResource(media_id)
+
+			ReturnAPIError(response, 500, "INTERNAL_ERROR", "Internal server error, Check the logs for details.")
+			return
+		}
 
 		GetVault().media.ReleaseMediaResource(media_id)
+	} else {
+		// Put the description into the media assets
 
-		ReturnAPIError(response, 500, "INTERNAL_ERROR", "Internal server error, Check the logs for details.")
-		return
+		media := GetVault().media.AcquireMediaResource(media_id)
+
+		if media == nil {
+			ReturnAPIError(response, 404, "NOT_FOUND", "Media not found")
+			return
+		}
+
+		meta, err := media.StartWrite(session.key)
+
+		if err != nil {
+			LogError(err)
+
+			GetVault().media.ReleaseMediaResource(media_id)
+
+			ReturnAPIError(response, 500, "INTERNAL_ERROR", "Internal server error, Check the logs for details.")
+			return
+		}
+
+		if meta == nil {
+			media.CancelWrite()
+			GetVault().media.ReleaseMediaResource(media_id)
+			ReturnAPIError(response, 404, "NOT_FOUND", "Media not found")
+			return
+		}
+
+		// Change metadata
+
+		if meta.HasExtendedDescription {
+			// Remove old asset
+			oldAsset := meta.ExtendedDescriptionAsset
+			success, asset_path, asset_lock := media.AcquireAsset(oldAsset, ASSET_SINGLE_FILE)
+
+			if success {
+				asset_lock.RequestWrite()
+				asset_lock.StartWrite()
+
+				os.Remove(asset_path)
+
+				asset_lock.EndWrite()
+
+				media.ReleaseAsset(oldAsset)
+			}
+		}
+
+		meta.HasExtendedDescription = false
+
+		// Save
+		err = media.EndWrite(meta, session.key, false)
+
+		if err != nil {
+			LogError(err)
+
+			GetVault().media.ReleaseMediaResource(media_id)
+
+			ReturnAPIError(response, 500, "INTERNAL_ERROR", "Internal server error, Check the logs for details.")
+			return
+		}
+
+		GetVault().media.ReleaseMediaResource(media_id)
 	}
-
-	GetVault().media.ReleaseMediaResource(media_id)
 
 	// Response
 
