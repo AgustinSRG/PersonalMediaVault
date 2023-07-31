9c2f5d257637d287a2cb65c1dd38b86a391c1f73
AgustinSRG <agustinsanromanguzman@gmail.com>
2023-07-31 19:53:13 +0200

    Bump version
    1.8.5

--- backend/main.go
+++ backend/main.go
@@ -12,7 +12,7 @@ import (
 	child_process_manager "github.com/AgustinSRG/go-child-process-manager"
 )
 
-const BACKEND_VERSION = "1.8.4"
+const BACKEND_VERSION = "1.8.5"
 
 type BackendOptions struct {
 	debug       bool // Debug mode
