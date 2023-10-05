0e19fde6e721411fbb3d20f52e54b10a2c0b770e
AgustinSRG <agustinsanromanguzman@gmail.com>
2023-10-05 21:04:38 +0200

    Frontend: Fix issue with service worker navigator

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -44,7 +44,7 @@ export default defineConfig({
                 ],
             },
             workbox: {
-                navigateFallbackAllowlist: [/^\/(\?.*)?$/],
+                navigateFallbackAllowlist: [/^\/$/],
             },
         }),
     ],
