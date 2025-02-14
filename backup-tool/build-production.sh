35f11dcede4d8ae57870b6ba4c031b96ac689dd9
AgustinSRG <agustinsanromanguzman@gmail.com>
2025-02-14 18:04:32 +0100

    Add extra flags to linux build scripts

--- backup-tool/build-production.sh
+++ backup-tool/build-production.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-go build -ldflags="-s -w" -o pmv-backup
+CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -ldflags="-s -w" -o pmv-backup
--- launcher/build-production.sh
+++ launcher/build-production.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-go build -ldflags="-s -w" -o pmv
+CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -ldflags="-s -w" -o pmv
