7f48f2e4533fcb737cf90081d42fddb8d34ae7a1
AgustinSRG <agustinsanromanguzman@gmail.com>
2025-05-10 12:12:49 +0200

    Bump version (1.22.0)

--- backend/main.go
+++ backend/main.go
@@ -13,7 +13,7 @@ import (
 	child_process_manager "github.com/AgustinSRG/go-child-process-manager"
 )
 
-const BACKEND_VERSION = "1.21.6"
+const BACKEND_VERSION = "1.22.0"
 
 type BackendOptions struct {
 	debug       bool // Debug mode
--- launcher/main.go
+++ launcher/main.go
@@ -15,7 +15,7 @@ import (
 	"github.com/nicksnyder/go-i18n/v2/i18n"
 )
 
-const VERSION = "1.21.6"
+const VERSION = "1.22.0"
 
 // Program entry point
 func main() {
