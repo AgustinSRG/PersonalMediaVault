925fc5d9c400f4d56fe1da4f800deb97c8c4c755
Agustín San Román <agustinsrg@air-institute.com>
2025-09-28 12:52:17 +0200

    Launcher: Compute default vault location

--- launcher-gui/src/control/vault_closed.rs
+++ launcher-gui/src/control/vault_closed.rs
@@ -4,12 +4,15 @@ use std::sync::mpsc::Sender;
 
 use slint::ComponentHandle;
 
-use crate::{worker::LauncherWorkerMessage, MainWindow};
+use crate::{utils::get_default_vault_path, worker::LauncherWorkerMessage, MainWindow};
 
 pub fn setup_callbacks_vault_closed(ui: &MainWindow, worker_sender: Sender<LauncherWorkerMessage>) {
-     ui.on_open_default_vault({
+    ui.on_open_default_vault({
+        let worker_sender_c = worker_sender.clone();
         move || {
-            println!("TODO");
+            let _ = worker_sender_c.send(LauncherWorkerMessage::OpenVault {
+                path: get_default_vault_path(),
+            });
         }
     });
 
--- launcher-gui/src/utils/mod.rs
+++ launcher-gui/src/utils/mod.rs
@@ -2,3 +2,6 @@
 
 mod open_url;
 pub use open_url::*;
+
+mod default_vault_path;
+pub use default_vault_path::*;
