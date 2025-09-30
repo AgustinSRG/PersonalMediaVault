bb8cec5fb487ad6606e2b77566ef640cf8e24b5b
Agustín San Román <agustinsrg@air-institute.com>
2025-09-30 20:28:33 +0200

    Launcher: Update GUI

--- launcher-gui/src/control/mod.rs
+++ launcher-gui/src/control/mod.rs
@@ -13,4 +13,10 @@ use crate::{worker::LauncherWorkerMessage, MainWindow};
 pub fn setup_callbacks(ui: &MainWindow, worker_sender: Sender<LauncherWorkerMessage>) {
     setup_callbacks_about(ui);
     setup_callbacks_vault_closed(ui, worker_sender);
+
+    ui.on_close_launcher({
+        move || {
+            let _ = slint::quit_event_loop();
+        }
+    });
 }
--- launcher-gui/src/worker/mod.rs
+++ launcher-gui/src/worker/mod.rs
@@ -9,10 +9,14 @@ mod message;
 pub use message::*;
 
 mod tasks;
-use slint::Weak;
+use slint::{ComponentHandle, LogicalSize, Weak};
 pub use tasks::*;
 
-use crate::MainWindow;
+use crate::{
+    models::{FFmpegBadInstallationError, FFmpegConfig},
+    utils::load_ffmpeg_config,
+    FatalErrorType, LauncherStatus, MainWindow,
+};
 
 /// Run the launcher main worker thread
 pub fn run_worker_thread(
@@ -21,22 +25,45 @@ pub fn run_worker_thread(
     window_handle: Weak<MainWindow>,
 ) -> JoinHandle<()> {
     // Spawn thread
-    spawn(move || loop {
-        match receiver.recv() {
-            Ok(msg) => match msg {
-                LauncherWorkerMessage::OpenVault { path } => {
-                    println!("Open vault: {path}");
-                }
-                LauncherWorkerMessage::SelectVaultFolder => {
-                    select_vault_folder(sender.clone(), window_handle.clone());
-                }
-                LauncherWorkerMessage::Finish => {
+    spawn(move || {
+        let ffmpeg_config = match load_ffmpeg_config() {
+            Ok(c) => c,
+            Err(e) => {
+                let wh = window_handle.clone();
+                let _ = slint::invoke_from_event_loop(move || {
+                    let win = wh.unwrap();
+                    win.set_launcher_status(LauncherStatus::FatalError);
+                    match e {
+                        FFmpegBadInstallationError::FFmpegMissing => {
+                            win.set_fatal_error_type(FatalErrorType::FfmpegMissing);
+                        }
+                        FFmpegBadInstallationError::FFprobeMissing => {
+                            win.set_fatal_error_type(FatalErrorType::FfprobeMissing);
+                        }
+                    }
+                });
+
+                FFmpegConfig::default_config()
+            }
+        };
+
+        loop {
+            match receiver.recv() {
+                Ok(msg) => match msg {
+                    LauncherWorkerMessage::OpenVault { path } => {
+                        println!("Open vault: {path}");
+                    }
+                    LauncherWorkerMessage::SelectVaultFolder => {
+                        select_vault_folder(sender.clone(), window_handle.clone());
+                    }
+                    LauncherWorkerMessage::Finish => {
+                        return;
+                    }
+                },
+                Err(err) => {
+                    eprintln!("Error: {}", err);
                     return;
                 }
-            },
-            Err(err) => {
-                eprintln!("Error: {}", err);
-                return;
             }
         }
     })
