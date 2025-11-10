cdbc9e1449a820ba21f9258eaf67e7a3f1ee08af
Agustín San Román <agustinsrg@air-institute.com>
2025-11-10 17:18:17 +0100

    Launcher: Improve translation finder script

--- launcher-gui/find-translations.sh
+++ launcher-gui/find-translations.sh
@@ -1,3 +1,27 @@
 #/bin/bash
 
-find -name \*.slint | xargs slint-tr-extractor --join-existing -d "pmv-gui" --package-name "pmv-gui" -o translations/es/LC_MESSAGES/pmv-gui.po
+PKG_NAME="pmv-gui"
+LOCALES_DIR="translations"
+
+update_translation_file() {
+    local locale=$1
+    sed -i "s/^#:.*/#:/" translations/$locale/LC_MESSAGES/$PKG_NAME.po
+    find -name \*.slint | xargs slint-tr-extractor --join-existing -d "$PKG_NAME" --package-name "$PKG_NAME" -o translations/es/LC_MESSAGES/$PKG_NAME.po
+}
+
+# Find locales
+find "$LOCALES_DIR" -maxdepth 1 -type d -print0 | while IFS= read -r -d $'\0' folder; do
+    # Skip the parent directory itself
+    if [ "$folder" = "$LOCALES_DIR" ]; then
+        continue
+    fi
+
+    locale_to_process=`echo $folder | cut -d / -f 2`
+
+    echo "Processing locale: $locale_to_process"
+
+    update_translation_file $locale_to_process
+done
+
+
+
