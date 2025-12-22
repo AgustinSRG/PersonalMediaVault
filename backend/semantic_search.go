e5be24eec0fe17091798a49e61fe4ed605f36f97
Agustín San Román <agustinsrg@air-institute.com>
2025-12-22 11:42:26 +0100

    Backend: Implement SemanticSearchSystem (part 2)

--- backend/semantic_search.go
+++ backend/semantic_search.go
@@ -15,7 +15,14 @@ import (
 	"sync"
 	"time"
 
+	"github.com/google/uuid"
 	"github.com/qdrant/go-client/qdrant"
+)
+
+const (
+	QDRANT_FIELD_MEDIA       = "m"
+	QDRANT_FIELD_VECTOR_TYPE = "t"
+	QDRANT_FIELD_DATA_HASH   = "h"
 )
 
 // Semantic search configuration
@@ -290,7 +297,7 @@ func (s *SemanticSearchSystem) initializeInternal() {
 
 				_, err := s.qdrantClient.CreateFieldIndex(context.Background(), &qdrant.CreateFieldIndexCollection{
 					CollectionName: s.qDrantCollectionName,
-					FieldName:      "media",
+					FieldName:      QDRANT_FIELD_MEDIA,
 					FieldType:      &mediaFieldType,
 				})
 
@@ -321,10 +328,10 @@ type ClipEncodeTextRequest struct {
 }
 
 type ClipVectorResponse struct {
-	Features []float64 `json:"features"`
+	Features []float32 `json:"features"`
 }
 
-func (s *SemanticSearchSystem) clipEncodeTextInternal(text string) ([]float64, error) {
+func (s *SemanticSearchSystem) clipEncodeTextInternal(text string) ([]float32, error) {
 	body := ClipEncodeTextRequest{
 		Text: text,
 	}
@@ -361,11 +368,11 @@ func (s *SemanticSearchSystem) clipEncodeTextInternal(text string) ([]float64, e
 }
 
 // Encodes text into a vector
-func (s *SemanticSearchSystem) ClipEncodeText(text string) ([]float64, error) {
+func (s *SemanticSearchSystem) ClipEncodeText(text string) ([]float32, error) {
 	return s.clipEncodeTextInternal(text)
 }
 
-func (s *SemanticSearchSystem) clipEncodeImageInternal(image []byte) ([]float64, error) {
+func (s *SemanticSearchSystem) clipEncodeImageInternal(image []byte) ([]float32, error) {
 	resp, err := http.Post(s.clipEncodeImageUrl, "application/json", bytes.NewReader(image))
 
 	if err != nil {
@@ -395,6 +402,193 @@ func (s *SemanticSearchSystem) clipEncodeImageInternal(image []byte) ([]float64,
 // image - Bytes of the image file
 // Note: Make sure the file is not too big
 // The file must be validated before calling this function
-func (s *SemanticSearchSystem) ClipEncodeImage(image []byte) ([]float64, error) {
+func (s *SemanticSearchSystem) ClipEncodeImage(image []byte) ([]float32, error) {
 	return s.clipEncodeImageInternal(image)
+}
+
+// Indexed vector types
+type QdrantIndexedVectorType int
+
+const (
+	VECTOR_TYPE_TEXT  QdrantIndexedVectorType = 0
+	VECTOR_TYPE_IMAGE QdrantIndexedVectorType = 1
+)
+
+// Indexed vector
+type QdrantIndexedVector struct {
+	// Point ID
+	Id *qdrant.PointId
+	// The media ID
+	Media uint64
+	// The type of vector
+	VectorType QdrantIndexedVectorType
+	// A hash of the data
+	DataHash string
+}
+
+func QdrantIndexedVectorFromScoredPoint(p *qdrant.ScoredPoint) *QdrantIndexedVector {
+	mediaId := uint64(0)
+	vectorType := VECTOR_TYPE_TEXT
+	dataHash := ""
+
+	if p.Payload != nil {
+		mediaIdVal := p.Payload[QDRANT_FIELD_MEDIA]
+
+		if mediaIdVal != nil {
+			mediaId = uint64(mediaIdVal.GetIntegerValue())
+		}
+
+		vectorTypeVal := p.Payload[QDRANT_FIELD_VECTOR_TYPE]
+
+		if vectorTypeVal != nil {
+			vectorType = QdrantIndexedVectorType(vectorTypeVal.GetIntegerValue())
+		}
+
+		dataHashVal := p.Payload[QDRANT_FIELD_DATA_HASH]
+
+		if dataHashVal != nil {
+			dataHash = dataHashVal.GetStringValue()
+		}
+	}
+
+	return &QdrantIndexedVector{
+		Id:         p.Id,
+		Media:      mediaId,
+		VectorType: vectorType,
+		DataHash:   dataHash,
+	}
+}
+
+// Finds all the indexed vectors for a specific media
+func (s *SemanticSearchSystem) GetIndexedVectors(ctx context.Context, media uint64) ([]*QdrantIndexedVector, error) {
+	queryLimit := uint64(10)
+
+	searchResult, err := s.qdrantClient.Query(ctx, &qdrant.QueryPoints{
+		CollectionName: s.qDrantCollectionName,
+		Query:          nil,
+		Filter: &qdrant.Filter{
+			Must: []*qdrant.Condition{
+				qdrant.NewMatchInt(QDRANT_FIELD_MEDIA, int64(media)),
+			},
+		},
+		WithPayload: qdrant.NewWithPayload(true),
+		Limit:       &queryLimit,
+	})
+
+	if err != nil {
+		return nil, err
+	}
+
+	result := make([]*QdrantIndexedVector, len(searchResult))
+
+	for i := range searchResult {
+		result[i] = QdrantIndexedVectorFromScoredPoint(searchResult[i])
+	}
+
+	return result, nil
+}
+
+// Semantic search query
+type SemanticSearchQuery struct {
+	// The vector
+	Vector []float32
+
+	// The vector type (optional)
+	VectorType *QdrantIndexedVectorType
+
+	// Number of results to skip
+	Offset uint64
+
+	// Max number of results to get
+	Limit uint64
+}
+
+// Performs a vector query to the Qdrant database
+func (s *SemanticSearchSystem) QueryVectors(ctx context.Context, query *SemanticSearchQuery) ([]*QdrantIndexedVector, error) {
+	var queryFilter *qdrant.Filter = nil
+
+	if query.VectorType != nil {
+		queryFilter = &qdrant.Filter{
+			Must: []*qdrant.Condition{
+				qdrant.NewMatchInt(QDRANT_FIELD_VECTOR_TYPE, int64(*query.VectorType)),
+			},
+		}
+	}
+
+	queryOffset := &query.Offset
+
+	if *queryOffset == 0 {
+		queryOffset = nil
+	}
+
+	searchResult, err := s.qdrantClient.Query(ctx, &qdrant.QueryPoints{
+		CollectionName: s.qDrantCollectionName,
+		Query:          qdrant.NewQuery(query.Vector...),
+		Filter:         queryFilter,
+		WithPayload:    qdrant.NewWithPayload(true),
+		Limit:          &query.Limit,
+		Offset:         queryOffset,
+	})
+
+	if err != nil {
+		return nil, err
+	}
+
+	result := make([]*QdrantIndexedVector, len(searchResult))
+
+	for i := range searchResult {
+		result[i] = QdrantIndexedVectorFromScoredPoint(searchResult[i])
+	}
+
+	return result, nil
+}
+
+// Deletes all vector associated with a media asset
+func (s *SemanticSearchSystem) DeleteVectors(ctx context.Context, media uint64) error {
+	_, err := s.qdrantClient.Delete(ctx, &qdrant.DeletePoints{
+		CollectionName: s.qDrantCollectionName,
+		Points: &qdrant.PointsSelector{
+			PointsSelectorOneOf: &qdrant.PointsSelector_Filter{
+				Filter: &qdrant.Filter{
+					Must: []*qdrant.Condition{
+						qdrant.NewMatchInt(QDRANT_FIELD_MEDIA, int64(media)),
+					},
+				},
+			},
+		},
+	})
+
+	return err
+}
+
+// Inserts a vector into the Qdrant database
+// media - ID of the associated media
+// vectorType - Vector type
+// dataHash - Data hash
+// vector - The vector
+func (s *SemanticSearchSystem) InsertVector(ctx context.Context, media uint64, vectorType QdrantIndexedVectorType, dataHash string, vector []float32) error {
+	id, err := uuid.NewV7()
+
+	if err != nil {
+		return err
+	}
+
+	idStr := id.String()
+
+	_, err = s.qdrantClient.Upsert(ctx, &qdrant.UpsertPoints{
+		CollectionName: s.qDrantCollectionName,
+		Points: []*qdrant.PointStruct{
+			{
+				Id:      qdrant.NewIDUUID(idStr),
+				Vectors: qdrant.NewVectors(vector...),
+				Payload: qdrant.NewValueMap(map[string]any{
+					QDRANT_FIELD_MEDIA:       int64(media),
+					QDRANT_FIELD_VECTOR_TYPE: int64(vectorType),
+					QDRANT_FIELD_DATA_HASH:   dataHash,
+				}),
+			},
+		},
+	})
+
+	return err
 }
