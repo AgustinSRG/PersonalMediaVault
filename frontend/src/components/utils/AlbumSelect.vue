e484846e78f418a4628f73378845df3d0df54258
Agustín San Román <agustinsrg@air-institute.com>
2026-02-03 17:51:16 +0100

    Frontend: Refactor AlbumSelect into composition API

--- frontend/src/components/utils/AlbumSelect.vue
+++ frontend/src/components/utils/AlbumSelect.vue
@@ -1,7 +1,7 @@
 <template>
-    <div class="album-selector-container" tabindex="0" :class="{ expanded: expanded }" @keydown="onKeyDown">
+    <div ref="container" class="album-selector-container" tabindex="0" :class="{ expanded: expanded }" @keydown="onKeyDown">
         <div class="album-selector" :class="{ expanded: expanded, disabled: disabled }" @click="toggleExpand">
-            <div class="album-selected-name">{{ getAlbumName(album, albumsListUpdateCount) }}</div>
+            <div class="album-selected-name">{{ albumName }}</div>
             <div class="album-selector-chevron">
                 <div class="chevron"></div>
             </div>
@@ -10,6 +10,7 @@
         <div v-if="expanded" class="album-selector-suggestions-container">
             <div class="name-filter-input-container">
                 <input
+                    ref="nameFilterInput"
                     v-model="filter"
                     type="text"
                     class="form-control form-control-full-width name-filter-input"
@@ -19,7 +20,7 @@
                     @keydown="onInputKeyDown"
                 />
             </div>
-            <div class="album-selector-suggestions" @scroll.passive="onSuggestionsScroll">
+            <div ref="suggestionsContainer" class="album-selector-suggestions" @scroll.passive="onSuggestionsScroll">
                 <a
                     v-for="s in suggestions"
                     :key="s.id"
@@ -39,15 +40,20 @@
     </div>
 </template>
 
-<script lang="ts">
+<script setup lang="ts">
+import { onApplicationEvent } from "@/composables/on-app-event";
+import { useI18n } from "@/composables/use-i18n";
+import { useTimeout } from "@/composables/use-timeout";
 import { AlbumsController } from "@/control/albums";
 import { EVENT_NAME_ALBUMS_LIST_UPDATE } from "@/control/app-events";
 import { getFrontendUrl } from "@/utils/api";
 import { BigListScroller } from "@/utils/big-list-scroller";
 import { FocusTrap } from "@/utils/focus-trap";
 import { filterToWords, matchSearchFilter, normalizeString } from "@/utils/normalize";
-import { useVModel } from "@/utils/v-model";
-import { defineComponent, nextTick } from "vue";
+import { nextTick, onBeforeUnmount, onMounted, ref, useTemplateRef, watch } from "vue";
+
+// Translation function
+const { $t } = useI18n();
 
 /**
  * Filtered album item
@@ -69,277 +75,322 @@ interface AlbumItemFiltered {
     contains: boolean;
 }
 
-export default defineComponent({
-    name: "AlbumSelect",
-    props: {
-        album: Number,
-        disabled: Boolean,
+const props = defineProps({
+    /**
+     * True if the control is disabled
+     */
+    disabled: Boolean,
+});
+
+// Album ID model
+const album = defineModel<number>("album", {
+    default: -1,
+});
+
+/**
+ * Gets the name of a selected album
+ * @param id Album ID
+ * @returns Album name
+ */
+const getAlbumName = (id: number): string => {
+    if (id < 0) {
+        return "--";
+    }
+
+    const album = AlbumsController.AlbumsMap.get(id);
+
+    if (!album) {
+        return "";
+    }
+
+    return album.name;
+};
+
+// Album name
+const albumName = ref(getAlbumName(album.value));
+
+watch(album, () => {
+    albumName.value = getAlbumName(album.value);
+});
+
+// Full list of albums
+let albums = AlbumsController.GetAlbumsListMin();
+
+// When the albums list changes, update everything
+onApplicationEvent(EVENT_NAME_ALBUMS_LIST_UPDATE, () => {
+    albums = AlbumsController.GetAlbumsListMin();
+
+    albumName.value = getAlbumName(album.value);
+
+    updateSuggestions();
+});
+
+// Album title filter
+const filter = ref("");
+
+// Album suggestions
+const suggestions = ref<AlbumItemFiltered[]>([]);
+
+// Max number of items that will fit in the visible section of the scroller
+const LIST_SCROLLER_ITEMS_FIT = 9;
+
+// Scroller for the suggestions
+const bigListScroller = new BigListScroller(BigListScroller.GetWindowSize(LIST_SCROLLER_ITEMS_FIT), {
+    get: () => {
+        return suggestions.value;
     },
-    emits: ["update:album"],
-    setup(props) {
-        return {
-            albums: AlbumsController.GetAlbumsListMin(),
-            albumState: useVModel(props, "album"),
-
-            bigListScroller: null as BigListScroller<AlbumItemFiltered>,
-            focusTrap: null as FocusTrap,
-
-            blurTimeout: null as ReturnType<typeof setTimeout> | null,
-            filterChangedTimeout: null as ReturnType<typeof setTimeout> | null,
-        };
-    },
-    data: function () {
-        return {
-            suggestions: [] as AlbumItemFiltered[],
-            filter: "",
-
-            expanded: false,
-
-            albumsListUpdateCount: 0,
-        };
-    },
-    mounted: function () {
-        this.$listenOnAppEvent(EVENT_NAME_ALBUMS_LIST_UPDATE, this.onAlbumsListUpdated.bind(this));
-
-        this.focusTrap = new FocusTrap(this.$el, this.close.bind(this));
-
-        this.bigListScroller = new BigListScroller(BigListScroller.GetWindowSize(9), {
-            get: (): any[] => {
-                return this.suggestions;
-            },
-            set: (list) => {
-                this.suggestions = list;
-            },
-        });
-    },
-    beforeUnmount: function () {
-        if (this.focusTrap) {
-            this.focusTrap.destroy();
-        }
-
-        if (this.blurTimeout) {
-            clearTimeout(this.blurTimeout);
-        }
-
-        if (this.filterChangedTimeout) {
-            clearTimeout(this.filterChangedTimeout);
-        }
-    },
-    methods: {
-        onAlbumsListUpdated: function () {
-            this.albums = AlbumsController.GetAlbumsListMin();
-            this.updateSuggestions();
-            this.albumsListUpdateCount++;
-        },
-
-        getAlbumName: function (id: number, _updateCount: number): string {
-            if (id < 0) {
-                return "--";
-            }
-
-            const album = AlbumsController.AlbumsMap.get(id);
-
-            if (!album) {
-                return "";
-            }
-
-            return album.name;
-        },
-
-        onSuggestionsScroll: function (e: Event) {
-            this.bigListScroller.checkElementScroll(e.target as HTMLElement);
-        },
-
-        getSuggestionURL: function (s: AlbumItemFiltered): string {
-            return getFrontendUrl({
-                album: s.id,
-            });
-        },
-
-        clickSuggestion: function (s: AlbumItemFiltered, e: Event) {
-            e.preventDefault();
-            e.stopPropagation();
-
-            this.albumState = s.id;
-            this.closeInstantly();
-            this.$el.focus();
-        },
-
-        onFilterChanged: function () {
-            if (this.filterChangedTimeout) {
-                return;
-            }
-            this.filterChangedTimeout = setTimeout(() => {
-                this.filterChangedTimeout = null;
-                this.updateSuggestions();
-            }, 200);
-        },
-
-        onKeyDown: function (event: KeyboardEvent) {
-            if (event.key === "Enter") {
-                event.preventDefault();
-                event.stopPropagation();
-                this.toggleExpand();
-            }
-        },
-
-        onInputKeyDown: function (event: KeyboardEvent) {
-            if (event.key === "ArrowDown" && this.suggestions.length > 0) {
-                event.preventDefault();
-                const suggestionElement = this.$el.querySelector(".album-selector-suggestion");
-                if (suggestionElement) {
-                    suggestionElement.focus();
-                }
-            } else if (event.key === "Escape") {
-                event.preventDefault();
-                this.closeInstantly();
-                this.$el.focus();
-            }
-            event.stopPropagation();
-        },
-
-        onSuggestionKeyDown: function (event: KeyboardEvent) {
-            const target = event.target as HTMLElement;
-            if (event.key === "Enter") {
-                event.preventDefault();
-                event.stopPropagation();
-                target.click();
-            } else if (event.key === "ArrowUp") {
-                event.preventDefault();
-                event.stopPropagation();
-                const sibling = target.previousElementSibling as HTMLElement;
-                if (sibling && sibling.focus) {
-                    sibling.focus();
-                } else {
-                    const searchInput = this.$el.querySelector(".name-filter-input");
-                    if (searchInput) {
-                        searchInput.focus();
-                    }
-                }
-            } else if (event.key === "ArrowDown") {
-                event.preventDefault();
-                event.stopPropagation();
-                const sibling = target.nextElementSibling as HTMLElement;
-                if (sibling && sibling.focus) {
-                    sibling.focus();
-                }
-            } else if (event.key === "Escape") {
-                event.preventDefault();
-                event.stopPropagation();
-                this.closeInstantly();
-                this.$el.focus();
-            }
-        },
-
-        updateSuggestions: function () {
-            const albumFilter = normalizeString(this.filter).trim().toLowerCase();
-            const albumFilterWords = filterToWords(albumFilter);
-
-            const suggestions: AlbumItemFiltered[] = this.albums
-                .map((a) => {
-                    const i = albumFilter ? matchSearchFilter(a.name, albumFilter, albumFilterWords) : 0;
-                    return {
-                        id: a.id,
-                        name: a.name,
-                        nameLowerCase: a.nameLowerCase,
-                        starts: i === 0,
-                        contains: i >= 0,
-                    };
-                })
-                .filter((a) => {
-                    return a.starts || a.contains;
-                })
-                .sort((a, b) => {
-                    if (a.starts && !b.starts) {
-                        return -1;
-                    } else if (b.starts && !a.starts) {
-                        return 1;
-                    } else if (a.nameLowerCase < b.nameLowerCase) {
-                        return -1;
-                    } else {
-                        return 1;
-                    }
-                });
-
-            if (!this.filter) {
-                suggestions.unshift({
-                    id: -1,
-                    name: "--",
-                    nameLowerCase: "--",
-                    contains: true,
-                    starts: true,
-                });
-            }
-
-            this.bigListScroller.reset();
-            this.bigListScroller.addElements(suggestions);
-
-            nextTick(() => {
-                const elem = this.$el.querySelector(".album-selector-suggestions");
-                if (elem) {
-                    elem.scrollTop = 0;
-                }
-            });
-        },
-
-        toggleExpand: function () {
-            if (this.expanded) {
-                this.closeInstantly();
-                this.$el.focus();
-            } else if (!this.disabled) {
-                this.expand();
-            }
-        },
-
-        expand: function () {
-            if (this.blurTimeout) {
-                clearTimeout(this.blurTimeout);
-                this.blurTimeout = null;
-            }
-            this.expanded = true;
-            this.updateSuggestions();
-            if (this.focusTrap) {
-                this.focusTrap.activate();
-            }
-            AlbumsController.Load();
-
-            nextTick(() => {
-                const elem = this.$el.querySelector(".name-filter-input");
-
-                if (elem) {
-                    elem.focus();
-                    elem.select();
-                }
-            });
-        },
-
-        closeInstantly: function () {
-            if (this.blurTimeout) {
-                clearTimeout(this.blurTimeout);
-                this.blurTimeout = null;
-            }
-            this.blurTimeout = null;
-            this.expanded = false;
-            this.filter = "";
-            if (this.focusTrap) {
-                this.focusTrap.deactivate();
-            }
-        },
-
-        close: function () {
-            if (this.blurTimeout) {
-                clearTimeout(this.blurTimeout);
-                this.blurTimeout = null;
-            }
-            this.blurTimeout = setTimeout(() => {
-                this.blurTimeout = null;
-                this.expanded = false;
-                this.filter = "";
-                if (this.focusTrap) {
-                    this.focusTrap.deactivate();
-                }
-            }, 10);
-        },
+    set: (list) => {
+        suggestions.value = list;
     },
 });
+
+/**
+ * Event handler for 'scroll' on the suggestions container
+ * @param e The scroll event
+ */
+const onSuggestionsScroll = (e: Event) => {
+    bigListScroller.checkElementScroll(e.target as HTMLElement);
+};
+
+// Container element for suggestions
+const suggestionsContainer = useTemplateRef("suggestionsContainer");
+
+/**
+ * Updates the album suggestions based on the filter
+ * typed by the user
+ */
+const updateSuggestions = () => {
+    filterChangedTimeout.clear();
+
+    const albumFilter = normalizeString(filter.value).trim().toLowerCase();
+    const albumFilterWords = filterToWords(albumFilter);
+
+    const suggestions: AlbumItemFiltered[] = albums
+        .map((a) => {
+            const i = albumFilter ? matchSearchFilter(a.name, albumFilter, albumFilterWords) : 0;
+            return {
+                id: a.id,
+                name: a.name,
+                nameLowerCase: a.nameLowerCase,
+                starts: i === 0,
+                contains: i >= 0,
+            };
+        })
+        .filter((a) => {
+            return a.starts || a.contains;
+        })
+        .sort((a, b) => {
+            if (a.starts && !b.starts) {
+                return -1;
+            } else if (b.starts && !a.starts) {
+                return 1;
+            } else if (a.nameLowerCase < b.nameLowerCase) {
+                return -1;
+            } else {
+                return 1;
+            }
+        });
+
+    if (!filter.value) {
+        suggestions.unshift({
+            id: -1,
+            name: "--",
+            nameLowerCase: "--",
+            contains: true,
+            starts: true,
+        });
+    }
+
+    bigListScroller.reset();
+    bigListScroller.addElements(suggestions);
+
+    nextTick(() => {
+        if (suggestionsContainer.value) {
+            // Reset scroll of the suggestions container
+            suggestionsContainer.value.scrollTop = 0;
+        }
+    });
+};
+
+// Timeout to update the suggestions
+const filterChangedTimeout = useTimeout();
+
+// Delay to update the album suggestions (milliseconds)
+const UPDATE_SUGGESTIONS_DELAY = 200;
+
+/**
+ * Call when the filter changes
+ */
+const onFilterChanged = () => {
+    if (filterChangedTimeout.isSet()) {
+        return;
+    }
+
+    filterChangedTimeout.set(updateSuggestions, UPDATE_SUGGESTIONS_DELAY);
+};
+
+// Focus trap
+let focusTrap: null | FocusTrap = null;
+
+// Timeout to blur the element and hide the suggestions
+const blurTimeout = useTimeout();
+
+// True if the suggestions are visible
+const expanded = ref(false);
+
+// Reference to the input element
+const nameFilterInput = useTemplateRef("nameFilterInput");
+
+const expand = () => {
+    blurTimeout.clear();
+
+    expanded.value = true;
+
+    updateSuggestions();
+
+    focusTrap?.activate();
+
+    AlbumsController.Load();
+
+    nextTick(() => {
+        nameFilterInput.value?.focus();
+        nameFilterInput.value?.select();
+    });
+};
+
+/**
+ * Closes the suggestions instantly
+ */
+const closeInstantly = () => {
+    blurTimeout.clear();
+
+    expanded.value = false;
+
+    filter.value = "";
+
+    focusTrap?.deactivate();
+};
+
+// Delay to blur the element
+const BLUR_DELAY = 10;
+
+/**
+ * Closes the suggestions
+ */
+const close = () => {
+    blurTimeout.set(closeInstantly, BLUR_DELAY);
+};
+
+/**
+ * Toggles the expanded status
+ */
+const toggleExpand = () => {
+    if (expanded.value) {
+        closeInstantly();
+        container.value?.focus();
+    } else if (!props.disabled) {
+        expand();
+    }
+};
+
+onMounted(() => {
+    focusTrap = new FocusTrap(container.value, close);
+});
+
+onBeforeUnmount(() => {
+    focusTrap?.destroy();
+});
+
+/**
+ * Gets the URL of a suggestion
+ * @param s The suggestion item
+ * @returns The URL
+ */
+const getSuggestionURL = (s: AlbumItemFiltered): string => {
+    return getFrontendUrl({
+        album: s.id,
+    });
+};
+
+// Main container of the component
+const container = useTemplateRef("container");
+
+/**
+ * Event handler for 'click on a suggestion'
+ * @param s The suggestion item
+ * @param e The click event
+ */
+const clickSuggestion = (s: AlbumItemFiltered, e: Event) => {
+    e.preventDefault();
+    e.stopPropagation();
+
+    album.value = s.id;
+    closeInstantly();
+    container.value?.focus();
+};
+
+/**
+ * Handler for 'keydown' on the container
+ * @param event The keyboard event
+ */
+const onKeyDown = (event: KeyboardEvent) => {
+    if (event.key === "Enter") {
+        event.preventDefault();
+        event.stopPropagation();
+        toggleExpand();
+    }
+};
+
+/**
+ * Handler for 'keydown' on the input text element
+ * @param event The keyboard event
+ */
+const onInputKeyDown = (event: KeyboardEvent) => {
+    if (event.key === "ArrowDown" && suggestions.value.length > 0) {
+        event.preventDefault();
+        const firstSuggestionElement = container.value?.querySelector(".album-selector-suggestion") as HTMLElement;
+        if (firstSuggestionElement) {
+            firstSuggestionElement.focus();
+        }
+    } else if (event.key === "Escape") {
+        event.preventDefault();
+        closeInstantly();
+        container.value?.focus();
+    }
+    event.stopPropagation();
+};
+
+/**
+ * Handler for 'keydown' on a suggestion element
+ * @param event The keyboard event
+ */
+const onSuggestionKeyDown = (event: KeyboardEvent) => {
+    const target = event.target as HTMLElement;
+    if (event.key === "Enter") {
+        event.preventDefault();
+        event.stopPropagation();
+        target.click();
+    } else if (event.key === "ArrowUp") {
+        event.preventDefault();
+        event.stopPropagation();
+        const sibling = target.previousElementSibling as HTMLElement;
+        if (sibling && sibling.focus) {
+            sibling.focus();
+        } else {
+            nameFilterInput.value?.focus();
+        }
+    } else if (event.key === "ArrowDown") {
+        event.preventDefault();
+        event.stopPropagation();
+        const sibling = target.nextElementSibling as HTMLElement;
+        if (sibling && sibling.focus) {
+            sibling.focus();
+        }
+    } else if (event.key === "Escape") {
+        event.preventDefault();
+        event.stopPropagation();
+        closeInstantly();
+        container.value?.focus();
+    }
+};
 </script>
